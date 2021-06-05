8fd6eb865e93a669a13cab37bb28142cff331be6
Enes Arat <enes_arat@outlook.com>
2021-06-05 11:27:02 +0300

    8th question:Created a route for editing Student information

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/edit-student',function () {
+    echo "Route created for Student Information Editing";
+});
