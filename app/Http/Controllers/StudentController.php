20cbe2cebb0cf02723e8074e4fd8b5ab092c3225
Enes Arat <enes_arat@outlook.com>
2021-06-05 12:33:20 +0300

    13th question:Created a controller function to save the Student information

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,5 +7,18 @@ use App\Models\Student;
 
 class StudentController extends Controller
 {
-    //
+    public function formIndex(){
+        return view("createStudent");
+    }
+    public function formPost(Request $requested){
+
+        Student::create([
+            "name"=>$requested->stuName,
+            "surname"=>$requested->stuSurname,
+            "department"=>$requested->stuDepartment,
+        ]);
+        
+        echo "Student registration has been successfully completed.";
+        
+    }
 }
